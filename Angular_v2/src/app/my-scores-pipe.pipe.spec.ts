c2614810faa934dc9277896a98b4feea68f1b4ae
Tomek <JANISZTAT@GMAIL.COM>
2022-07-31 12:41:46 +0200

    Delete Angular_v2 directory

--- Angular_v2/src/app/my-scores-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyScoresPipePipe } from './my-scores-pipe.pipe';
-
-describe('MyScoresPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MyScoresPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
